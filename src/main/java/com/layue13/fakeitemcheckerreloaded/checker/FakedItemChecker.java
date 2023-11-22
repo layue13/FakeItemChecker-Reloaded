80de60da166dfddc71e4f8a9c85eb256d008383e
layue13 <layue13@protonmail.com>
2023-11-22 18:02:30 +0800

    feat core: add new method to check Inventory.

--- src/main/java/com/layue13/fakeitemcheckerreloaded/checker/FakedItemChecker.java
+++ src/main/java/com/layue13/fakeitemcheckerreloaded/checker/FakedItemChecker.java
@@ -1,18 +1,26 @@
 package com.layue13.fakeitemcheckerreloaded.checker;
 
 import com.google.common.base.Preconditions;
+import com.layue13.fakeitemcheckerreloaded.FakeItemCheckerReloaded;
 import com.layue13.fakeitemcheckerreloaded.dao.Repository;
+import com.layue13.fakeitemcheckerreloaded.entity.Log;
 import com.layue13.fakeitemcheckerreloaded.entity.Rule;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.UUID;
 import java.util.function.Function;
 
 public class FakedItemChecker {
-    private final Repository<Rule,Long> ruleRepository;
+    private final Repository<Rule, Long> ruleRepository;
 
-    public FakedItemChecker(Repository<Rule,Long> ruleRepository) {
+    public FakedItemChecker(Repository<Rule, Long> ruleRepository) {
         Preconditions.checkNotNull(ruleRepository);
         this.ruleRepository = ruleRepository;
     }
@@ -30,7 +38,33 @@ public class FakedItemChecker {
 
     public void check(Player holder, ItemStack[] itemStacks, Function<Player, Void> doAfterIfError) {
         Arrays.stream(itemStacks).forEach(itemStack -> {
-            check(holder,itemStack,doAfterIfError);
+            check(holder, itemStack, doAfterIfError);
+        });
+    }
+
+    public void checkInventory(FakeItemCheckerReloaded plugin, Event event, Player player, Inventory inventory, Collection<String> ignoredTitles) {
+        if (ignoredTitles.contains(inventory.getTitle())) {
+            return;
+        }
+        ItemStack[] inventoryItemStacks = inventory.getContents().clone();
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            check(player, inventoryItemStacks, p -> {
+                Log log = Log.builder()
+                        .id(UUID.randomUUID())
+                        .playerName(player.getName())
+                        .time(new Date())
+                        .server(Bukkit.getServer().getServerName())
+                        .location(player.getLocation().toString())
+                        .event(event.getEventName())
+                        .inventoryType(inventory.getType())
+                        .build();
+                plugin.getLogRepository().save(log);
+                plugin.getLogger().info(log.toString());
+                Bukkit.getScheduler().runTask(plugin, () -> {
+                    player.setBanned(true);
+                });
+                return null;
+            });
         });
     }
 }
